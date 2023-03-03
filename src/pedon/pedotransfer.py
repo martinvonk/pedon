841c9bbbd34fbbe0c3b46030127076af41e2d034
Martin Vonk <vonk.mart@gmail.com>
2023-03-03 18:05:05 +0100

    remove dumb variable k names

--- src/pedon/pedotransfer.py
+++ src/pedon/pedotransfer.py
@@ -96,27 +96,13 @@ def wosten(sm: SoilSample, ts: bool = False):
     return theta_r, theta_s, exp(ks_), exp(alpha_), exp(n_), exp(l_)
 
 
-def cosby(
-    sm: SoilSample,
-):
-    k01 = 3.100
-    k02 = 15.70
-    k03 = 0.300
-    k04 = 0.505
-    k05 = 0.037
-    k06 = 0.142
-    k07 = 2.170
-    k08 = 0.630
-    k09 = 1.580
-    k10 = 0.600
-    k11 = 0.640
-    k12 = 1.260
+def cosby(sm: SoilSample):
     c = sm.clay_p / 100
     s = sm.sand_p / 100
-    b = k01 + k02 * c - k03 * s
-    theta_s = k04 - k05 * c - k06 * s
-    psi_s = 0.01 * 10 ** (k07 - k08 * c - k09 * s)
-    ks = 10 ** (-k10 - k11 * c + k12 * s) * 25.2 / 3600
+    b = 3.100 + 15.70 * c - 0.300 * s
+    theta_s = 0.505 - 0.037 * c - 0.142 * s
+    psi_s = 0.01 * 10 ** (2.170 - 0.630 * c - 1.580 * s)
+    ks = 10 ** (-0.600 - 0.640 * c + 1.260 * s) * 25.2 / 3600
     theta_r = 0.0
     labda = 1 / b
     ks = ks * 8640000 / 1000  # kg/m2/s to cm/d
