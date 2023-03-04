96439cee359a66a852d42f0959062a5466d0768b
Martin Vonk <vonk.mart@gmail.com>
2023-03-04 16:37:29 +0100

    add two other wosten ptf functions

--- src/pedon/pedotransfer.py
+++ src/pedon/pedotransfer.py
@@ -14,9 +14,13 @@ class SoilSample:
     th33: float  # cm
     th1500: float  # cm
     om_p: float  # organic matter %
+    m50: float  # median sand fraction
 
 
 def wosten(sm: SoilSample, ts: bool = False) -> Genuchten:
+    """Wosten et al (1999) - Development and use of a database of hydraulic
+    properties of European soils"""
+
     topsoil = 1.0 * ts
 
     theta_s = (
@@ -104,7 +108,117 @@ def wosten(sm: SoilSample, ts: bool = False) -> Genuchten:
     )
 
 
+def wosten_sand(sm: SoilSample, ts: bool = False) -> Genuchten:
+    """Wosten et al. (2001) - Waterretentie- en doorlatendheidskarakteristieken
+    van boven- en ondergronden in Nederland: de Staringreeks"""
+
+    topsoil = 1.0 * ts
+    theta_s = (
+        -35.7
+        - 0.1843 * sm.rho
+        - 0.03576 * sm.m50
+        + 0.0000261 * sm.m50**2
+        - 0.0564 * sm.silt_p**-1
+        + 0.008 * sm.om_p**-1
+        + 496.0 * sm.m50**-1
+        + 0.02244 * log(sm.om_p)
+        + 7.56 * log(sm.m50)
+    )
+
+    k_s = (
+        45.8
+        - 14.34 * sm.rho
+        + 0.001481 * sm.silt_p**2
+        - 27.5 * sm.rho**-1
+        - 0.891 * log(sm.silt_p)
+        - 0.34 * log(sm.om_p)
+    )
+
+    alpha = (
+        13.66
+        - 5.91 * sm.rho
+        - 0.172 * topsoil
+        + 0.003248 * sm.m50
+        - 11.89 * sm.rho**-1
+        - 2.121 * sm.silt_p**-1
+        - 0.3742 * log(sm.silt_p)
+    )
+
+    n = (
+        -1.057
+        + 0.1003 * sm.om_p
+        + 1.119 * sm.rho
+        + 0.000764 * sm.silt_p**2
+        - 0.1397 * sm.om_p**-1
+        - 57.2 * sm.m50**-1
+        - 0.557 * log(sm.om_p)
+        - 0.02997 * sm.rho * sm.silt_p
+    )
+
+    l = (
+        -76.4
+        - 0.097 * sm.silt_p
+        + 59.6 * sm.rho
+        + 0.0332 * sm.m50
+        - 13.45 * sm.rho**2
+        + 0.001127 * sm.silt_p**2
+        + 0.00431 * sm.om_p**2
+        - 0.0000399 * sm.m50**2
+        + 40.8 * sm.rho**-1
+        + 2.364 * sm.silt_p**-1
+        + 1.014 * log(sm.silt_p)
+    )
+
+    return Genuchten(k_s=k_s, theta_r=0.01, theta_s=theta_s, alpha=alpha, n=n, l=l)
+
+
+def wosten_clay(sm: SoilSample) -> Brooks:
+    """Wosten et al. (2001) - Waterretentie- en doorlatendheidskarakteristieken
+    van boven- en ondergronden in Nederland: de Staringreeks"""
+
+    theta_s = (
+        0.6311
+        + 0.003383 * sm.clay_p
+        - 0.09699 * sm.rho**2
+        - 0.00204 * sm.rho * sm.clay_p
+    )
+
+    k_s = (
+        -42.6
+        + 8.71 * sm.om_p
+        + 61.9 * sm.rho
+        - 20.79 * sm.rho**2
+        - 0.2107 * sm.om_p**2
+        - 0.01622 * sm.clay_p * sm.om_p
+        - 5.382 * sm.rho * sm.om_p
+    )
+
+    alpha = (
+        -19.13
+        + 0.812 * sm.om_p
+        + 23.4 * sm.rho
+        - 8.16 * sm.rho**2
+        + 0.423 * sm.om_p**-1
+        + 2.388 * log(sm.om_p)
+        - 1.338 * sm.rho * sm.om_p
+    )
+
+    n = (
+        -0.235
+        + 0.972 * sm.rho**-1
+        - 0.7743 * log(sm.clay_p)
+        - 0.3154 * log(sm.om_p)
+        + 0.0678 * sm.rho_p * sm.om_p
+    )
+
+    l = 0.102 + 0.0222 * sm.clay_p - 0.043 * sm.rho * sm.clay_p
+
+    return Genuchten(k_s=k_s, theta_r=0.01, theta_s=theta_s, alpha=alpha, n=n, l=l)
+
+
 def cosby(sm: SoilSample) -> Brooks:
+    """Cooper (2021) - Using data assimilation to optimize pedotransfer
+    functions using field-scale in situ soil moisture observations"""
     c = sm.clay_p / 100
     s = sm.sand_p / 100
     b = 3.100 + 15.70 * c - 0.300 * s
