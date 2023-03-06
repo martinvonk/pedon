3d9f9b56a63e47d86d5d7e5eef4abac71921610a
Martin Vonk <vonk.mart@gmail.com>
2023-03-07 00:36:58 +0100

    add sample to soil

--- src/pedon/__init__.py
+++ src/pedon/__init__.py
@@ -1,3 +1,4 @@
 from . import pedotransfer, soil, soilmodel
 from .soil import Soil
 from .soilmodel import Brooks, Fredlund, Gardner, Genuchten, Sorab, plot_hcf, plot_swrc
+from .pedotransfer import SoilSample
--- src/pedon/soil.py
+++ src/pedon/soil.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Type
+from pandas import read_csv, read_excel
 
-from pandas import read_csv
-
-from .soilmodel import Brooks, Fredlund, Gardner, Genuchten, SoilModel, Sorab
+from .soilmodel import Genuchten, SoilModel
+from .pedotransfer import SoilSample
 
 
 @dataclass
@@ -11,25 +12,28 @@ class Soil:
     name: str
     type: str | None = None
     model: SoilModel | None = None
+    sample: SoilSample | None = None
     source: str | None = None
     description: str | None = None
 
+    def from_name(self, sm: Type[SoilModel]) -> "Soil":
+        path = Path(__file__).parent / f"datasets/{sm.__name__}.csv"
+        ser = read_csv(path, index_col=["name"]).loc[self.name].to_dict()
+        self.__setattr__("type", ser.pop("soil type"))
+        self.__setattr__("source", ser.pop("source"))
+        self.__setattr__("description", ser.pop("description"))
+        self.__setattr__("model", sm(**ser))
+        return self
 
-def get_soil(name: str, soilmodelname: str = "Genuchten") -> Soil:
-    sms = {
-        "Genuchten": Genuchten,
-        "Brooks": Brooks,
-        "Gardner": Gardner,
-        "Sorab": Sorab,
-        "Fredlund": Fredlund,
-    }
-    path = Path(__file__).parent / f"datasets/{soilmodelname}.csv"
-    ser = read_csv(path, index_col=["name"]).loc[name].to_dict()
-    soil = Soil(
-        name=name,
-        type=ser.pop("soil type"),
-        source=ser.pop("source"),
-        description=ser.pop("description"),
-    )
-    soil.__setattr__("model", sms[soilmodelname](**ser))
-    return soil
+    def from_staring(self, year: str = "2018") -> "Soil":
+        path = Path(__file__).parent / f"datasets/Staring_{year}.xlsx"
+        parameters = read_excel(path, sheet_name="parameters", index_col=0)
+        ser = parameters.loc[self.name].to_dict()
+        self.__setattr__("type", ser.pop("soil type"))
+        self.__setattr__("source", ser.pop("source"))
+        self.__setattr__("description", ser.pop("description"))
+        sm = Genuchten(**ser)
+        self.__setattr__("model", sm)
+        ss = SoilSample().from_staring(name=self.name, year=year)
+        self.__setattr__("sample", ss)
+        return self
