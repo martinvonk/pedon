5138492bcf7f96481e361915f54b3b66d9740455
Martin Vonk <vonk.mart@gmail.com>
2023-03-23 16:24:07 +0100

    Update __init__.py
    https://github.com/microsoft/pylance-release/issues/2953#issuecomment-1168408943

--- src/pedon/__init__.py
+++ src/pedon/__init__.py
@@ -2,5 +2,9 @@
 # flake8: noqa
 from . import _params, soil, soilmodel
 from ._version import __version__
-from .soil import Soil, SoilSample
-from .soilmodel import Brooks, Fredlund, Gardner, Genuchten, Panday, plot_hcf, plot_swrc
+from .soil import Soil as Soil
+from .soil import SoilSample as SoilSample
+from .soilmodel import Brooks as Brooks
+from .soilmodel import Genuchten as Genuchten
+from .soilmodel import Panday as Panday
+from .soilmodel import plot_hcf, plot_swrc
