82bcd98574d6f45cec82ca040892c997e975c2e9
Martin Vonk <vonk.mart@gmail.com>
2023-12-01 14:04:25 +0100

    Update __init__.py

--- src/pedon/__init__.py
+++ src/pedon/__init__.py
@@ -5,7 +5,7 @@ from ._version import __version__
 from .soil import Soil as Soil
 from .soil import SoilSample as SoilSample
 from .soilmodel import Brooks as Brooks
+from .soilmodel import Gardner as Gardner
 from .soilmodel import Genuchten as Genuchten
 from .soilmodel import Panday as Panday
-from .soilmodel import Gardner as Gardner
 from .soilmodel import plot_hcf, plot_swrc
