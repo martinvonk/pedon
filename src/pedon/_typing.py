a027913cba2de7f12acf4449f58effc6d5ec7f56
Martin Vonk <vonk.mart@gmail.com>
2024-12-17 11:03:56 +0100

    Update _typing.py

--- src/pedon/_typing.py
+++ src/pedon/_typing.py
@@ -1,4 +1,4 @@
-from numpy import float64
-from numpy.typing import NDArray
+# from numpy import float64
+from numpy.typing import ArrayLike
 
-FloatArray = NDArray[float64]
+FloatArray = ArrayLike #NDArray[float64]
