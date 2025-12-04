b038e4bf188f0ee894bac91c219f63464aedd83e
Aaron Peche <aaron.peche@bgr.de>
2025-12-04 10:55:36 +0100

    Changes in __init__.py to pass RUFF tests

--- src/pedon/__init__.py
+++ src/pedon/__init__.py
@@ -8,9 +8,9 @@ from .soil import SoilSample as SoilSample
 from .soilmodel import Brooks as Brooks
 from .soilmodel import Fredlund as Fredlund
 from .soilmodel import Gardner as Gardner
-from .soilmodel import Mod_Gardner as Mod_Gardner
 from .soilmodel import Genuchten as Genuchten
 from .soilmodel import Haverkamp as Haverkamp
+from .soilmodel import Mod_Gardner as Mod_Gardner
 from .soilmodel import Panday as Panday
 from .soilmodel import plot_hcf as plot_hcf
 from .soilmodel import plot_swrc as plot_swrc
